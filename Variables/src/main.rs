d69e93a77e0a8078b547b41d682b9c9c03392bdb
Mohamed-Olwi311222 <mohamedolwi38@gmail.com>
2024-12-04 20:35:10 +0200

    Added shadowing in the program

--- Variables/src/main.rs
+++ Variables/src/main.rs
@@ -9,4 +9,15 @@ fn main() {
     println!("The value of immutable_var is {immutable_var}" );
     println!("The value of mutable_var is {mutable_var}" );
     println!("The value of THREE_HOURS_IN_SECONDS is {THREE_HOURS_IN_SECONDS}");
+    /*----------- SHADOWING-------- */
+    let shadow_var = 10;
+    println!("Shadow_var is {shadow_var}");
+    let shadow_var = 20; // shadowing
+    println!("Shadow_var2 is {shadow_var}");
+    {
+        //local shadowing
+        let shadow_var = 30;
+        println!("local Shadow_var is {shadow_var}");
+    }
+    println!("Shadow_var2 is {shadow_var}"); //same as before the local shadowing
 }
